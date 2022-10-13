49f4cac9600e722f4e8226893e2e6cc489d01c16
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2022-10-13 18:52:09 +0200

    Add slides to Selenium presentation

--- Web Scraping with Selenium/scraper.R
+++ Web Scraping with Selenium/scraper.R
@@ -1,12 +1,11 @@
 rm(list = ls())
 
 library(RSelenium)
-library(xml2)
 
 
 ## Set up ----
 # Setting up the driver
-chromeDr <- rsDriver(browser = "chrome", port = 4566L, chromever = "105.0.5195.52", # you will have to adjust this version
+chromeDr <- rsDriver(browser = "chrome", port = 4567L, chromever = "106.0.5249.61", # you will have to adjust this version
                      extraCapabilities = list(chromeOptions = list(args = c('--disable-gpu', '--window-size=1280,800'),
                                                                    prefs = list(
                                                                      "profile.default_content_settings.popups" = 0L,
@@ -15,6 +14,77 @@ chromeDr <- rsDriver(browser = "chrome", port = 4566L, chromever = "105.0.5195.5
                                                                    ))))
 
 remDr <- chromeDr[["client"]]
+
+
+
+## Functions ----
+# Function to find child elements
+find_homegate_elements <- function(listing){
+  
+  # Get the href of the parent <a>
+  listing_id <- unlist(strsplit(unlist(listing$getElementAttribute("href")), "/(?!.*/)", perl = TRUE))[2]
+  
+  # Establish default values
+  price <- NA
+  sq_m <- NA
+  nr_rooms <- NA
+  location <- NA
+  description <- NA
+  
+  tryCatch(
+    expr = {
+      price <- listing$findChildElement(value = paste0("//*/a[contains(@href, '", listing_id, "')]//span[contains(@class, 'ListItemPrice_price')]/span[2]"))
+      price <- unlist(price$getElementText())
+    },
+    error = function(err){
+      return(1)
+    })
+  
+  tryCatch(
+    expr = {
+      suppressMessages({
+        sq_m <- listing$findChildElement(value = paste0("//*/a[contains(@href, '", listing_id, "')]//span[contains(@class, 'ListItemLivingSpace_value')]"))
+        sq_m <- unlist(sq_m$getElementText())
+      })
+    },
+    error = function(err){
+      return(1)
+    })
+  
+  tryCatch(
+    expr = {
+      nr_rooms <- listing$findChildElement(value = paste0("//*/a[contains(@href, '", listing_id, "')]//span[contains(@class, 'ListItemRoomNumber_value')]"))
+      nr_rooms <- unlist(nr_rooms$getElementText())
+    },
+    error = function(err){
+      return(1)
+    })
+  
+  tryCatch(
+    expr = {
+      location <- listing$findChildElement(value = paste0("//*/a[contains(@href, '", listing_id, "')]//div[contains(@class, 'ListItem') and contains(@class, '_data_')]/p[2]/span"))
+      location <- unlist(location$getElementText())
+    },
+    error = function(err){
+      return(1)
+    })
+  
+  tryCatch(
+    expr = {
+      description <- listing$findChildElement(value = paste0("//*/a[contains(@href, '", listing_id, "')]//div[contains(@class, 'ListItemDescription_description')]/p"))
+      description <- unlist(description$getElementText())
+    },
+    error = function(err){
+      return(1)
+    })
+  
+  return(data.frame("price" = price,
+                    "sq_m" = sq_m,
+                    "nr_rooms" = nr_rooms,
+                    "location" = location,
+                    "description" = description,
+                    "listing_id" = listing_id))
+}
 
 
 
@@ -32,47 +102,36 @@ homegate_data <- data.frame()
 # Go through the listings by canton
 for (c_link in canton_links){
   
+  # Navigate to the page
+  remDr$navigate(c_link)
+  
   # Go through all the pages per canton
   while (last_page == FALSE){
     
-    # Get the price
-    e <- remDr$findElements(value = "//*/span[contains(@class, 'ListItemPrice_price')]/span[2]")
-    prices <- unlist(lapply(e, function(x){x$getElementText()}))
+    # Get all the listing wrappers on the page
+    parents <- remDr$findElements(value = "//*/a[contains(@class, 'ResultList_ListItem')]")
     
-    # Get the square meters
-    e <- remDr$findElements(value = "//*/span[contains(@class, 'ListItemLivingSpace_value')]")
-    sq_m <- unlist(lapply(e, function(x){x$getElementText()}))
+    # Get the values from the child elements
+    page_result <- lapply(parents, find_homegate_elements)
     
-    # Get the nr. of rooms
-    e <- remDr$findElements(value = "//*/span[contains(@class, 'ListItemRoomNumber_value')]")
-    nr_rooms <- unlist(lapply(e, function(x){x$getElementText()}))
+    # Coerce the results into a data frame
+    page_result <- as.data.frame(do.call(rbind, page_result))
     
-    # Get the location
-    e <- remDr$findElements(value = "//*/div[contains(@class, 'ListItem') and contains(@class, '_data_')]/p[2]/span")
-    location <- unlist(lapply(e, function(x){x$getElementText()}))
+    # Add a column for the listing type
+    page_result$listing_type <- "rent"
     
-    # Get the description
-    e <- remDr$findElements(value = "//*/div[contains(@class, 'ListItemDescription_description')]/p")
-    description <- unlist(lapply(e, function(x){x$getElementText()}))
-    
-    
-    # Write the data to the output table
-    homegate_data <- rbind(homegate_data,
-                           data.frame("canton_link" = c_link,
-                                      "type" = "rent",
-                                      "price" = prices,
-                                      "sq_m" = sq_m,
-                                      "nr_rooms" = nr_rooms,
-                                      "location" = location,
-                                      "description" = description))
+    # Join with the rest of the scraped data
+    homegate_data <- rbind(homegate_data, page_result)
     
     # Check if we're on the last page, otherwise go to next page
-    tryCatch(next_page_button <- remDr$findElements(value = "//*/svg[contains(@class, 'Icon') and contains(@class, 'disabledButton')]"))
+    tryCatch(
+      next_page_button <- remDr$findElements(value = "//*/p[contains(@class, 'HgPaginationSelector_nextPreviousArrow')]")
+    )
     
     if (length(next_page_button) < 1){
       last_page <- TRUE
     } else {
-      e <- remDr$findElement(value = "//*/a[contains(@class, 'router-link-active HgPaginationSelector_nextPreviousArrow') and contains(@aria-label, 'Go to next page')]")
+      e <- remDr$findElement(value = "//*/a[contains(@aria-label, 'Go to next page')]")
       e$clickElement()
     }
   }
@@ -84,64 +143,7 @@ for (c_link in canton_links){
 
 
 ## Listings to buy ----
-# Moving to the target page
-remDr$navigate("https://www.homegate.ch/buy/real-estate/switzerland")
-
-# Extract all the canton links
-e <- remDr$findElements(value = "//*/div[contains(@class, 'GeoDrillDownSRPLink')]/a")
-canton_links <- unlist(lapply(e, function(x){x$getElementAttribute("href")}))[2:27]
-
-# Go through the listings by canton
-for (c_link in canton_links){
-  
-  # Go through all the pages per canton
-  while (last_page == FALSE){
-    
-    # Get the price
-    e <- remDr$findElements(value = "//*/span[contains(@class, 'ListItemPrice_price')]/span[2]")
-    prices <- unlist(lapply(e, function(x){x$getElementText()}))
-    
-    # Get the square meters
-    e <- remDr$findElements(value = "//*/span[contains(@class, 'ListItemLivingSpace_value')]")
-    sq_m <- unlist(lapply(e, function(x){x$getElementText()}))
-    
-    # Get the nr. of rooms
-    e <- remDr$findElements(value = "//*/span[contains(@class, 'ListItemRoomNumber_value')]")
-    nr_rooms <- unlist(lapply(e, function(x){x$getElementText()}))
-    
-    # Get the location
-    e <- remDr$findElements(value = "//*/div[contains(@class, 'ListItem') and contains(@class, '_data_')]/p[2]/span")
-    location <- unlist(lapply(e, function(x){x$getElementText()}))
-    
-    # Get the description
-    e <- remDr$findElements(value = "//*/div[contains(@class, 'ListItemDescription_description')]/p")
-    description <- unlist(lapply(e, function(x){x$getElementText()}))
-    
-    
-    # Write the data to the output table
-    homegate_data <- rbind(homegate_data,
-                           data.frame("canton_link" = c_link,
-                                      "type" = "buy",
-                                      "price" = prices,
-                                      "sq_m" = sq_m,
-                                      "nr_rooms" = nr_rooms,
-                                      "location" = location,
-                                      "description" = description))
-    
-    # Check if we're on the last page, otherwise go to next page
-    tryCatch(next_page_button <- remDr$findElements(value = "//*/svg[contains(@class, 'Icon') and contains(@class, 'disabledButton')]"))
-    
-    if (length(next_page_button) < 1){
-      last_page <- TRUE
-    } else {
-      e <- remDr$findElement(value = "//*/a[contains(@class, 'router-link-active HgPaginationSelector_nextPreviousArrow') and contains(@aria-label, 'Go to next page')]")
-      e$clickElement()
-    }
-  }
-  
-  # Reset the last page indicator
-  last_page <- FALSE
-}
+# TODO
 
 
 
